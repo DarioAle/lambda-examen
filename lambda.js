26c6a96ad521f8c14e6a5fa7d174d5e4ae528b04
Darío Alejandro Arias Muñoz <33333224+DarioAle@users.noreply.github.com>
2021-05-03 08:55:39 -0500

    Create lambda.js

---
+++ lambda.js
@@ -0,0 +1,24 @@
+const healthPath = '/paciente';
+
+exports.handler = async function(event) {
+  console.log('Request event: ', event);
+  let response;
+  switch(true) {
+    case event.httpMethod === 'GET' && event.path === healthPath:
+      response = buildResponse(200);
+      break;
+    default:
+      response = buildResponse(404, '404 Not Found');
+  }
+  return response;
+}
+
+function buildResponse(statusCode, body) {
+    return {
+        statusCode: statusCode,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }
+}
